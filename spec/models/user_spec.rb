cd0b8dcab8475d028ef584a22e42b0ed319b750c
Adam Smith <adam.smith18@ntlworld.com>
2014-05-30 22:03:20 +0100

    User test spec up to date

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,69 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
+  describe "when username" do
+
+  	describe "is too short" do
+  		before { @user.username = "aa" }
+  		it { should_not be_valid}
+  	end
+
+  	describe "is too long" do
+  		before { @user.username = "a" * 21 }
+  		it { should_not be_valid }
+  	end
+
+  	describe "is not present" do
+  		before { @user.username = " " }
+  		it { should_not be_valid }
+  	end
+
+  	describe "is not valid" do
+
+  		describe "because of illegal characters" do
+  			usernames = %w[@Example ex.ample $Example 456Example]
+  				usernames.each do |invalid_username|
+  					before { @user.username = invalid_username } 
+  					it { should_not be_valid }
+  				end
+  			end
+  		end
+
+  		describe "because it already exists" do
+  			before do
+  				user_with_same_username = @user.dup
+  				user_with_same_username.username = @user.username.upcase
+  				user_with_same_username.save
+  			end
+  			it { should_not be_valid }
+  		end
+   	end
+
+  describe "when email" do
+
+  	describe "is not present" do
+  		before { @user.email = " " }
+  		it { should_not be_valid }
+  	end
+
+  	describe "already exists" do
+  		before do
+  			user_with_same_email = @user.dup
+  			user_with_same_email.email = @user.email.upcase
+  			user_with_same_email.save
+		end
+		it { should_not be_valid }
+	end
+
+	describe "is not valid" do
+		addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+  		addresses.each do |valid_address|
+  			before { @user.email = valid_address }
+  			it { should_not be_valid}
+  		end
+	end
+  end
 end
