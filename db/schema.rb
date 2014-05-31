58610517cb477f499e56cffb87d992f7d824d650
Adam Smith <adam.smith18@ntlworld.com>
2014-05-31 21:03:23 +0100

    Database migration and admin menu options in sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529214707) do
+ActiveRecord::Schema.define(version: 20140531195106) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140529214707) do
     t.string   "remember_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
