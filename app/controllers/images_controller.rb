ce350c39b3fbe3dd99f72de05bcfb3d6eb731ae2
Adam Smith <adam.smith18@ntlworld.com>
2014-06-01 22:32:41 +0100

    Created images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,22 @@
 class ImagesController < ApplicationController
 
+	def new
+		@image = Image.new
+	end
+
+	def create
+		if image_params.user_id
+			current_user.images.build(image_params)
+		else
+			current_user.bike.images.build(image_params)
+		end
+	end
+
+	def destroy
+	end
+
 	private
 		def image_params
 			params.require(:image).permit(:file_name)
+		end
 end
