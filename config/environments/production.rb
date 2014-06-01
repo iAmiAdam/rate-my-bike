11e1ee5ac9e0fc04a0a1feacf9d1036bc4d2125c
Adam Smith <adam.smith18@ntlworld.com>
2014-06-01 17:50:24 +0100

    Precompiled for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ RateMyBike::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Getting Font Awesome to precompile
+  config.assets.precompile += %w( *.css *.js *.woff *.eot *.svg *.ttf)
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
