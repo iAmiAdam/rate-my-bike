8cfd34ae7373d25231d7100419b27b2117db5c83
Adam Smith <adam.smith18@ntlworld.com>
2014-06-01 17:28:30 +0100

    Added edit to controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,5 +1,6 @@
 class BikesController < ApplicationController
 	before_action :signed_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: [:edit]
 
 	def index
 	end
@@ -18,10 +19,27 @@ class BikesController < ApplicationController
 		end
 	end
 
+	def edit
+		@bike = Bike.find_by(params[:id])
+	end
+
+	def update
+		@bike = Bike.find_by(params[:id])
+		if @bike.update_attributes(bike_params) then
+			redirect_to @bike
+		else
+			render 'edit'
+		end
+	end
+
 
 	private
 
 		def bike_params
 			params.require(:bike).permit(:make, :model, :name, :description, :category)
+		end
+
+		def correct_user
+			redirect_to root_url unless current_user?(@bike.user_id)
 		end
 end
