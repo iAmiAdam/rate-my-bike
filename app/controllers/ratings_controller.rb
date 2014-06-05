086aa6f36ab480a7b08645b22a3106106888868c
Adam Smith <adam.smith18@ntlworld.com>
2014-06-05 14:33:52 +0100

    Ratings now working, not limited properly

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,20 +1,21 @@
 class RatingsController < ApplicationController
 
 	def create
-		if Rating.where(user_id: current_user.id, bike_id: params[:bike_id], created_at: Time.now) === true then 
-			redirect_to bike_path(params[:bike_id]) 
+		if Rating.where(user_id: current_user.id, bike_id: params[:rating][:bike_id], created_at: Rating.today) === true then 
+			redirect_to bike_path(params[:rating][:bike_id]) 
 		else 
-			current_user.ratings.build(rating_params)
-			@bike = bike.find_by(params[:bike_id])
-			@bike.rating += params[:rating]
+			@rating = current_user.ratings.build(rating_params)
+			@rating.save
+			@bike = Bike.find_by(params[:rating][:bike_id])
+			@bike.rating += params[:rating][:score].to_i
 			@bike.rating = @bike.rating / 2
-			@bike.update_attribute(rating: @bike.rating)
-			redirect_to bike_path(params[:bike_id])
+			@bike.update_attribute(:rating, @bike.rating)
+			redirect_to bike_path(params[:rating][:bike_id])
 		end
 	end
 
 	private
 		def rating_params
-			params.permit(:bike_id, :score)
+			params.require(:rating).permit(:bike_id, :score)
 		end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,5 @@ class Rating < ActiveRecord::Base
 	belongs_to :bike
 	belongs_to :user
 	validates :score, presence: true, inclusion: { in: 1..10 }
+	scope :today, lambda { where("created_at >= ? and created_at <= ?", Date.today.beginning_of_day, Date.today.end_of_day) }
 end
