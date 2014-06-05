bc11453f24c0f504b025dff4ae40e1d0145d44bb
Adam Smith <adam.smith18@ntlworld.com>
2014-06-05 16:23:41 +0100

    Bike rating calculation now on the fly

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,16 +1,12 @@
 class RatingsController < ApplicationController
 
 	def create
-		if Rating.find(:all, conditions: { created_at: Time.now.day, bike_id: params[:rating][:bike_id]} ) === true then 
+		if Rating.find(:all, conditions: { created_at: Date.today(), bike_id: params[:rating][:bike_id]} ) === true then 
 			redirect_to bike_path(params[:rating][:bike_id]) 
 		else 
 			@rating = current_user.ratings.build(rating_params)
 			@rating.save
-			@bike = Bike.find_by(params[:rating][:bike_id])
-			@bike.rating += params[:rating][:score].to_i
-			@bike.rating = @bike.rating / 2
-			@bike.update_attribute(:rating, @bike.rating)
-			redirect_to bike_path(params[:rating][:bike_id])
+			
 		end
 	end
 
