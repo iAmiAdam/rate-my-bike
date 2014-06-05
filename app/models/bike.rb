7e991693fa4f94ccfdda35a9ec816f6825845ba4
Adam Smith <adam.smith18@ntlworld.com>
2014-06-05 23:55:54 +0100

    Helps if you press save

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -16,4 +16,13 @@ class Bike < ActiveRecord::Base
 
 
 	has_many :comments
+
+	def current_rating()
+		score = 0 
+		
+		Bike.ratings.each do |f|
+		score += Bike.ratings.score
+		end
+		score = score / Bike.ratings.count
+	end
 end
